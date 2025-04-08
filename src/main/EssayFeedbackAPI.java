5b2aa1622352ec55e64ebab1a76e4117f593f95b
chen0 <chen0@Michael-laptop>
2025-04-07 22:24:31 -0400

    Merge origin/master
    # Conflicts:
    #	.gitignore
    #	src/calculators/DateTimeCalculator.java

---
+++ src/main/EssayFeedbackAPI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+import java.net.HttpURLConnection;
+import org.json.JSONObject;
+import com.google.gson.Gson;
+
+/**
+ *
+ * @author chen0
+ */
+public class EssayFeedbackAPI {
+    
+}
