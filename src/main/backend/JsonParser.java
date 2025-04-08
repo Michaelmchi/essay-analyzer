d8164b4e4c3527b74c6297cd5866b8c60cb45d44
chen0 <chen0@Michael-laptop>
2025-04-08 14:07:56 -0400

    Merge origin/master
    # Conflicts:
    #	.gitignore
    #	src/calculators/DateTimeCalculator.java

--- src/main/backend/JsonParser.java
+++ src/main/backend/JsonParser.java
@@ -8,6 +8,14 @@ package main.backend;
  *
  * @author chen0
  */
+import com.google.gson.*;
+
 public class JsonParser {
-    
+
+    public static String extractReply(String jsonResponse) {
+        com.google.gson.JsonObject json = com.google.gson.JsonParser.parseString(jsonResponse).getAsJsonObject();
+        JsonArray choices = json.getAsJsonArray("choices");
+        JsonObject message = choices.get(0).getAsJsonObject().getAsJsonObject("message");
+        return message.get("content").getAsString().trim();
+    }
 }
